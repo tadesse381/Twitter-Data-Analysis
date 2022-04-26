4ce5e0df10cf5ee082fce776bbe4241a4bb11a2e
Tadesse-Kebede <85294225+tadesse381@users.noreply.github.com>
2022-04-26 13:28:19 +0300

    This is Third Commit

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -127,7 +127,7 @@ if __name__ == "__main__":
     # required column to be generated you should be creative and add more features
     columns = ['created_at', 'source', 'original_text','clean_text', 'sentiment','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
     'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
-    _, tweet_list = read_json("../Economic_Twitter_Data.json")
+    _, tweet_list = read_json("data/Economic_Twitter_Data.json")
     tweet = TweetDfExtractor(tweet_list)
     tweet_df = tweet.get_tweet_df() 
 
