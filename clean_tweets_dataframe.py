a924de59b7e27b18853ce3f03b76cd1dc1c9a392
Tadesse-Kebede <85294225+tadesse381@users.noreply.github.com>
2022-04-27 19:07:55 +0300

    Update clean_tweets_dataframe.py

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -5,33 +5,31 @@ class Clean_Tweets:
     """
     def __init__(self, df:pd.DataFrame):
         self.df = df
-        print('Automation in Action...!!!')
         
-    def drop_unwanted_column(self, df:pd.DataFrame)->pd.DataFrame:
+    def drop_unwanted_column(self, column_name)->pd.DataFrame:
         """
         remove rows that has column names. This error originated from
         the data collection stage.  
         """
-        unwanted_rows = df[df['retweet_count'] == 'retweet_count' ].index
-        df.drop(unwanted_rows , inplace=True)
-        df = df[df['polarity'] != 'polarity']
-        
+        df = self.df
+        df = df.drop([column_name], axis=1)
         return df
+
     def drop_duplicate(self, df:pd.DataFrame)->pd.DataFrame:
         """
         drop duplicate rows
         """
-        
-        pass
-        
-        #return df
+        df = self.df   
+        df.drop_duplicates()     
+        return df
+
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
         """
         convert column to datetime
         """
-        
-        df = df[df['created_at'] >= '2020-12-31' ]
-        
+        # df = df[df['created_at'] >= '2020-12-31' ]
+        df = self.df
+        df['created_at'] = pd.to_datetime(df['created_at'])
         return df
     
     def convert_to_numbers(self, df:pd.DataFrame)->pd.DataFrame:
@@ -39,16 +37,19 @@ class Clean_Tweets:
         convert columns like polarity, subjectivity, retweet_count
         favorite_count etc to numbers
         """
-        #df['polarity'] = pd.----
-        
-       
+        df = self.df
+        df['polarity'] = pd.to_numeric(df['polarity'])
+        df['subjectivity'] = pd.to_numeric(df['subjectivity'])
+        df['retweet_count'] = pd.to_numeric(df['retweet_count'])
+        df['favorite_count'] = pd.to_numeric(df['favorite_count'])
+
         return df
     
     def remove_non_english_tweets(self, df:pd.DataFrame)->pd.DataFrame:
         """
         remove non english tweets from lang
         """
-        
-        #df = ----
-        
+        df = self.df
+        df = df[df['language'] == 'en']
+
         return df
