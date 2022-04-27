298899606d0afa64adaf263edaaa73aed920559f
Tadesse-Kebede <85294225+tadesse381@users.noreply.github.com>
2022-04-27 19:08:55 +0300

    Update extract_dataframe.py
    Fifth Commit

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -1,6 +1,6 @@
 import json
 import pandas as pd
-#from textblob import TextBlob
+from textblob import TextBlob
 
 def read_json(json_file: str)->list:
     """
@@ -35,8 +35,9 @@ class TweetDfExtractor:
 
     # an example function
     def find_statuses_count(self)->list:
-        #statuses_count 
-        pass
+        user = [x.get('user', {}) for x in self.tweets_list]
+        statuses_count = [x.get('statuses_count', 0) for x in user]
+        return statuses_count
     def find_full_text(self)->list:
         text = [x.get('retweeted_status', {}).get('extended_tweet',{}).get('full_text', '') \
             for x in self.tweets_list]
@@ -45,23 +46,31 @@ class TweetDfExtractor:
     
     def find_sentiments(self, text)->list:
         
-        #return polarity, self.subjectivity
-        pass
+        text = [x.get('retweeted_status', {}) for x in self.tweets_list]
+        extended_tweet = [x.get('extended_tweet', {}) for x in text]
+        full_text = [x.get('full_text', '') for x in extended_tweet]
+
+        sentimentedText = [TextBlob(x) for x in full_text]
+        polarity = []
+        subjectivity = []
+        for i in range(len(sentimentedText)):
+            polarity.append(sentimentedText[i].sentiment.polarity)
+            subjectivity.append(sentimentedText[i].sentiment.subjectivity)
+        
+        return polarity, subjectivity
 
     def find_created_time(self)->list:
         created_time = [x.get('created_at', None) for x in self.tweets_list]
         return created_time
 
     def find_source(self)->list:
-        #source = 
-
-        #return source
-        pass
+       source = [x.get('source', '') for x in self.tweets_list]
+       return source
 
     def find_screen_name(self)->list:
-        #screen_name =
-        pass
-
+          users = [x.get('user', {}) for x in self.tweets_list]
+          screen_name = [x.get('screen_name') for x in users]
+          return screen_name
     def find_followers_count(self)->list:
         followers_count = [x.get('user', {}).get('followers_count', 0) for x in self.tweets_list]
         return followers_count
@@ -79,21 +88,26 @@ class TweetDfExtractor:
          fav_count = [x.get('retweeted_status', {}).get('favorite_count', 0) for x in self.tweets_list]
          return fav_count
     def find_retweet_count(self)->list:
-        #retweet_count =
-        pass
+        retweeted_status = [x.get('retweeted_status', {}) for x in self.tweets_list]
+        retweet_count = [x.get('retweet_count', None) for x in retweeted_status]
+
+        return retweet_count
 
     def find_hashtags(self)->list:
-        #hashtags =
-        pass
+        # return list of all None
+        hashtags = [x.get('hashtags', None) for x in self.tweets_list]
+        return hashtags
     def find_mentions(self)->list:
-        #mentions =
-        pass
-    
+       # return list of all None
+        hashtags = [x.get('hashtags', None) for x in self.tweets_list]
+        return hashtags
     def find_location(self)->list:
         location = [x.get('user', {}).get('location', None) for x in self.tweets_list]
         return location
-    
-    def get_tweet_df(self, save=False)->pd.DataFrame:
+    def find_lang(self)->list:
+         lang = [x.get('retweeted_status', {}).get('lang', None) for x in self.tweets_list]
+         return lang 
+    def get_tweet_df(self, save=True)->pd.DataFrame:
         """required column to be generated you should be creative and add more features"""
         
         columns = ['created_at', 'source', 'original_text','polarity','subjectivity', 'lang', 'favorite_count', 'retweet_count', 
